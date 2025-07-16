757ea2ec62cc46c432e613e2169d19db1a8fd894
fpiou <frpiou@gmail.com>
2025-07-16 10:59:47 +0200

    Refactor project structure and remove old HTML files
    - Updated Vite configuration to use root base for deployment on Netlify.
    - Deleted outdated HTML files (working-enhanced.html and working.html) to streamline the project.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',  // Utiliser des chemins relatifs
+  base: '/',  // Netlify utilise le domaine racine
   build: {
     outDir: 'dist',
   },
